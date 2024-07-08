f6f232ea4da813508f239e9ac29004e3f6ae6e4f
MD Pahlovi <mdpahlovi07@gmail.com>
2024-07-08 23:47:07 +0600

    [Fixed] Deploy Error

--- apps/client/src/types/index.ts
+++ apps/client/src/types/index.ts
@@ -1,42 +1,6 @@
 import { fabric } from "fabric";
-import { Gradient, Pattern } from "fabric/fabric-impl";
-
-export enum CursorMode {
-    Hidden,
-    Chat,
-    ReactionSelector,
-    Reaction,
-}
-
-export type CursorState =
-    | { mode: CursorMode.Hidden }
-    | { mode: CursorMode.Chat; message: string; previousMessage: string | null }
-    | { mode: CursorMode.ReactionSelector }
-    | { mode: CursorMode.Reaction; reaction: string; isPressed: boolean };
-
-export type Reaction = {
-    value: string;
-    timestamp: number;
-    point: { x: number; y: number };
-};
-
-export type ReactionEvent = {
-    x: number;
-    y: number;
-    value: string;
-};
 
 export type Pointer = { x: number; y: number };
-
-export type ShapeData = {
-    type: string;
-    width: number;
-    height: number;
-    fill: string | Pattern | Gradient;
-    left: number;
-    top: number;
-    objectId: string | undefined;
-};
 
 export type Attributes = {
     type: string;
@@ -155,7 +119,7 @@ export type CanvasObjectModified = {
 export type CanvasPathCreated = {
     roomRef: React.MutableRefObject<string | null>;
     setShape: (shape: fabric.Object) => void;
-    options: { path: fabric.Object };
+    options: any;
 };
 
 export type CanvasSelectionCreated = {
