a8f6e8ab56ab12316d2f44f72ff057b7e3501e89
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2024-12-04 22:43:16 +0600

    [Done] Redis user mannagement

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,12 @@
 import http from "http";
 import { createClient } from "redis";
 import { Server } from "socket.io";
+import { generateName } from "./utils/ulits.js";
 
 const httpServer = http.createServer();
-
-const client = createClient({ url: "rediss://red-ct5snqjqf0us738911hg:9NqvFG2h1iK7xD4BPBqdB9pbZyW6ayKz@singapore-redis.render.com:6379" });
+const client = createClient({
+    url: "rediss://red-ct5snqjqf0us738911hg:9NqvFG2h1iK7xD4BPBqdB9pbZyW6ayKz@singapore-redis.render.com:6379",
+});
 
 client.on("error", (error) => console.log("Redis Client Error", error));
 
@@ -13,12 +15,29 @@ await client.connect();
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
-    socket.on("join:room", ({ room }) => {
-        if (room) socket.join(room);
+    socket.on("join:room", async ({ room }) => {
+        if (room) {
+            socket.join(room);
+            await client.lPush(`${room}_users`, JSON.stringify({ id: socket.id, name: generateName() }));
+            io.to(room).emit("users", { users: await client.lRange(`${room}_users`, 0, -1) });
+        }
     });
 
-    socket.on("leave:room", ({ room }) => {
-        if (room) socket.leave(room);
+    socket.on("leave:room", async ({ room }) => {
+        if (room) {
+            socket.leave(room);
+            const users = await client.lRange(`${room}_users`, 0, -1);
+            const userIndex = users.findIndex((u) => JSON.parse(u).id === socket.id);
+
+            if (userIndex !== -1) await client.lRem(`${room}_users`, 0, users[userIndex]);
+
+            const remainingUsers = await client.lLen(`${room}_users`);
+            if (remainingUsers === 0) {
+                await client.del(`${room}_users`);
+            } else {
+                io.to(room).emit("users", { users: await client.lRange(`${room}_users`, 0, -1) });
+            }
+        }
     });
 
     socket.on("set:shape", ({ room, ...shape }) => {
@@ -43,6 +62,24 @@ io.on("connection", (socket) => {
     socket.on("cursor", ({ room, cursor }) => {
         if (room) socket.broadcast.to(room).emit("cursor", { id: socket.id, ...cursor });
     });
+
+    socket.on("disconnecting", async () => {
+        const rooms = Array.from(socket.rooms).filter((r) => r !== socket.id);
+
+        for (const room of rooms) {
+            const users = await client.lRange(`${room}_users`, 0, -1);
+            const userIndex = users.findIndex((u) => JSON.parse(u).id === socket.id);
+
+            if (userIndex !== -1) await client.lRem(`${room}_users`, 0, users[userIndex]);
+
+            const remainingUsers = await client.lLen(`${room}_users`);
+            if (remainingUsers === 0) {
+                await client.del(`${room}_users`);
+            } else {
+                io.to(room).emit("users", { users: await client.lRange(`${room}_users`, 0, -1) });
+            }
+        }
+    });
 });
 
-httpServer.listen(4000, () => console.log(`🚀 Server Running On http://localhost:${4000}`));
+httpServer.listen(4000, () => console.log(`🚀 Server Running On http://localhost:4000`));
